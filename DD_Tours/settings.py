0a1ce46e7e90b4d4a420d8c566ebd1ca9a8a7c55
pratishthabagohil <pratishthagohil57@gmail.com>
2025-03-06 17:02:09 +0530

    Initial commit - Django proje

--- DD_Tours/settings.py
+++ DD_Tours/settings.py
@@ -221,9 +221,10 @@ WSGI_APPLICATION = 'DD_Tours.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",  # Check this path
     }
 }
+
 
 # Authentication & Allauth Settings
 LOGIN_REDIRECT_URL = '/'
